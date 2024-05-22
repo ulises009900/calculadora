e2616904858ab1f4291932609f5b8dcf87d53f80
ulises009900 <comettou@gmail.com>
2024-05-22 08:10:06 -0300

    es el esqueleto

---
+++ calcu.py
@@ -0,0 +1,20 @@
+print("Suma {1}")
+print("resta {2}")
+print("multiplicasion {3}")
+print("division {4}")
+print("raiz {5}")
+
+A = int(input("elija su modo ; "))
+
+if A==1:
+
+elif A==2:
+
+elif A==3:
+
+elif A==4:
+
+elif A==5:
+
+else:
+    False
