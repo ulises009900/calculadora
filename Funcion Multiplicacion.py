595095ef25ece6f6c0846b61e20b657a753adfcc
Jorgantoni26 <jorgantoni26@hotmail.com>
2024-05-26 18:47:09 -0300

    Funcion Multiplicacion

---
+++ Funcion Multiplicacion.py
@@ -0,0 +1,5 @@
+#Funcion Multiplicacion
+A=input("Ingrese el primer numero a multiplicar")
+B=input("Ingrese el segundo numero a multiplicar")
+multiplicacion= A * B
+print("El resultado de la multiplicacion es  ",multiplicacion)
