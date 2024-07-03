12e5b200f9c434cbb36774c873724fc3cf432457
ulises009900 <comettou@gmail.com>
2024-07-03 08:13:04 -0300

    esta calcu es laes que anda

--- RESTA.py
+++ RESTA.py
@@ -1,4 +1,5 @@
-def resta(num1,num2):
-    return num1 - num2
-    ressultado = resta(10,5)
+def resta():
+    num1=int(input("ingrese su numero a restar"))
+    num2=int(input("ingrese su segundo numero a restar "))
+    ressultado = num1 - num2
     print("El resultado de la resta es:",ressultado)
